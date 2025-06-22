b2c1038140952c9d80becd12e9293c271a4bb154
victormoraes0 <victormoraeshugo13@gmail.com>
2025-06-22 17:03:22 -0300

    hello and status controllers commit

---
+++ src/main/java/org/example/controller/HelloController.java
@@ -0,0 +1,9 @@
+package org.example.controller;
+
+import io.javalin.Javalin;
+
+public class HelloController {
+    public static void routes(Javalin app) {
+        app.get("/hello",ctx -> ctx.result("Hello, Javalin!"));
+    }
+}
---
+++ src/main/java/org/example/controller/StatusController.java
@@ -0,0 +1,16 @@
+package org.example.controller;
+
+import io.javalin.Javalin;
+import java.time.Instant;
+import java.util.Map;
+
+public class StatusController {
+    public static void routes(Javalin app) {
+        app.get("/status", ctx -> {
+            ctx.json(Map.of(
+                    "status", "ok",
+                    "timestamp", Instant.now().toString()
+            ));
+        });
+    }
+}
